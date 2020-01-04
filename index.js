e50eb248b3ca02209a50763e43b5f69a1c38fdc2
jantapabh <jantapa2407@gmail.com>
2020-01-04 23:45:07 +0700

    test

--- index.js
+++ index.js
@@ -9,16 +9,41 @@
 
 // // คำสั่ง select คือการเลือกและ append คือการบอกว่าเอาแบบที่เป็น paragraph และ text คือข้อความที่ต้องการให้แสดง
 
-var dataset = [1, 2, 3, 4, 5];
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
 
-d3.select('body')
-    .selectAll('p')
-    .data(dataset)
-    .enter()
-    .append('p') //appends paragraph for each data element
-    // .text('D3 is awesome!!');
+var svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-    .text (function(d) {
-        return d;
-    });
+var barChart = svg.selectAll("rect")
+.data(dataset)
+.enter()
+.append("rect")
+.attr("y", function(d) {
+
+    return svgHeight - d
+})
+
+.attr("height", function(d) {
+    return d;
+})
+.attr("width", barWidth - barPadding)
+.attr("transform", function (d, i) {
+    var translate = [barWidth * i, 0];
+    return "translate("+ translate +")";
+});
+
+
+// d3.select('body')
+//     .selectAll('p')
+//     .data(dataset)
+//     .enter()
+//     .append('p') //appends paragraph for each data element
+//     // .text('D3 is awesome!!');
+
+//     .text(function (d) {
+//         return d;
+//     });
 
