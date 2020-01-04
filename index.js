cee0cbdc7e42395adccec700206911402a3fdd75
jantapabh <jantapa2407@gmail.com>
2020-01-04 23:24:39 +0700

    test

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-// javascript
+// // javascript
 
-d3.select();
-d3.selectAll();
-d3.select('h1').style('color', 'red')
-.attr('class', 'heading')
-.text('Update it');
-d3.select('body').append('p').text('First Paragraph');
+// d3.select();
+// d3.selectAll();
+// d3.select('h1').style('color', 'red')
+// .attr('class', 'heading')
+// .text('Update it');
+// d3.select('body').append('p').text('First Paragraph');
 
-// คำสั่ง select คือการเลือกและ append คือการบอกว่าเอาแบบที่เป็น paragraph และ text คือข้อความที่ต้องการให้แสดง
+// // คำสั่ง select คือการเลือกและ append คือการบอกว่าเอาแบบที่เป็น paragraph และ text คือข้อความที่ต้องการให้แสดง
 
+
+
+var da = taset = [1, 2, 3, 4, 5];
+
+d3.select('body')
+    .selectAll('p')
+    .data(dataset)
+    .enter()
+    .append('p') //appends paragraph for each data element
+    .text('D3 is awesome!!');
+
+    // // .text (function(d) {
+    //     return d3;
+    // });
 
