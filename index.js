b5c165fdebcfaf441909666114d3f390725fe3ca
jantapabh <jantapa2407@gmail.com>
2020-01-04 23:19:10 +0700

    test 3

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ d3.select('h1').style('color', 'red')
 .text('Update it');
 d3.select('body').append('p').text('First Paragraph');
 
-
+// คำสั่ง select คือการเลือกและ append คือการบอกว่าเอาแบบที่เป็น paragraph และ text คือข้อความที่ต้องการให้แสดง
 
 
