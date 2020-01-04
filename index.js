d5233387c3345146dd70b2697d97b6ccfa18cae4
jantapabh <jantapa2407@gmail.com>
2020-01-05 00:11:11 +0700

    edit

--- index.js
+++ index.js
@@ -22,25 +22,25 @@ var svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-    var xScale = d3.scaleLinear()
+var xScale = d3.scaleLinear()
     .domain([0, d3.max(dataset)])
     .range([0, svgWidth]);
 
-    var yScale = d3.scaleLinear()
+var yScale = d3.scaleLinear()
     .domain([0, d3.max(data)])
     .range([0, svgHeight, 0]);
 
-    var x_axis = d3.axisBottom().scale(xScale);
-    var y_axis = d3.axisLeft().scale(yScale);
+var x_axis = d3.axisBottom().scale(xScale);
+var y_axis = d3.axisLeft().scale(yScale);
 
-    svg.append("g")
+svg.append("g")
     .attr("transform", "translate(50, 10)")
     .call(y_axis);
 
-    var xAxisTranslate = svgHeight - 20;
+var xAxisTranslate = svgHeight - 20;
 
-    svg.append("g")
-    .attr("transform", "translate(50, " + xAxisTranslate +")")
+svg.append("g")
+    .attr("transform", "translate(50, " + xAxisTranslate + ")")
     .call(x_axis);
 
 // var barChart = svg.selectAll("rect")
