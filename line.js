7189ed5ba93428c18588218823b6971dd73fe8c9
jantapabh <jantapa2407@gmail.com>
2020-01-05 01:07:43 +0700

    axios

---
+++ line.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const url = "http://35.194.179.37:5000/api/pm?fbclid=IwAR3Yzg2xZBB3TXgGSSw_DmXt8aIdlru44N_F1oISXfpxiaiDZelNNsYxXf4";
+
+
+
+// ดึงข้อมูล pm 2.5 มาจาก api
+
+async function getPm(pmData) {
+
+    const result = [];
+    const response = await axios.get(url);
+    const data = response.data;
+
+    for (let i = data.length - 1; i >= 1; i--) {
+
+        const pmData = data[i].pm;
+        const timeData = data[i]["Timestamp"];
+        const arrData = timeData.split('T');
+
+
+        result.push(pmData);
+
+    }
+
+    // console.log(result);
+
+    // console.log(result.length);
+
+    return result;
+
+}
